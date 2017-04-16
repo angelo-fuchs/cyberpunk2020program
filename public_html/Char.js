7224065b5586590e4b11dff953c809ba428e9ad2
Angelo Fuchs <af@steamnet.de>
2017-04-16 11:38:27 +0200

    Code cleanup

--- public_html/Char.js
+++ public_html/Char.js
@@ -1,34 +1,22 @@
 function Char() {
-this.attributes = [
-{"name": "Body", "value": 0},
-{"name": "Ref", "value": 0},
-{"name": "Int", "value": 0},
-{"name": "Tech", "value": 0},
-{"name": "Luck", "value": 0},
-{"name": "Attr", "value": 0},
-{"name": "Cool", "value": 0},
-{"name": "Emp", "value": 0},
-{"name": "Move", "value": 0}
-];
+	this.attributes = [
+		{"name": "Body", "value": 0},
+		{"name": "Ref", "value": 0},
+		{"name": "Int", "value": 0},
+		{"name": "Tech", "value": 0},
+		{"name": "Luck", "value": 0},
+		{"name": "Attr", "value": 0},
+		{"name": "Cool", "value": 0},
+		{"name": "Emp", "value": 0},
+		{"name": "Move", "value": 0}
+	];
 
-this.base = [{"name": "Handle", "value": undefined},
-{"name":"Age", "value": undefined}];
+	this.base = [{"name": "Handle", "value": undefined},
+		{"name": "Age", "value": undefined}];
 
-this.skills = [];
-this.weapons = [];
-this.inventory = [];
-}
-
-function equalsIgnoreCase(strTerm, strToSearch) 
-{ 
-	strToSearch = strToSearch.toLowerCase(); 
-	strTerm = strTerm.toLowerCase(); 
-	if(strToSearch===strTerm) 
-	{ 
-		return true; 
-	} else { 
-		return false; 
-	} 
+	this.skills = [];
+	this.weapons = [];
+	this.inventory = [];
 }
 
 Char.prototype = {
@@ -38,7 +26,7 @@ Char.prototype = {
 			myBase = this.base[ii];
 			for (var jj in jsonObj.base) {
 				nextBase = jsonObj.base[jj];
-				if (equalsIgnoreCase(myBase.name, nextBase.name))
+				if (utilities.equalsIgnoreCase(myBase.name, nextBase.name))
 					myBase.value = nextBase.value;
 			}
 		}
@@ -46,7 +34,7 @@ Char.prototype = {
 			myAttr = this.attributes[ii];
 			for (var jj in jsonObj.attributes) {
 				nextAttr = jsonObj.attributes[jj];
-				if (equalsIgnoreCase(myAttr.name, nextAttr.name))
+				if (utilities.equalsIgnoreCase(myAttr.name, nextAttr.name))
 					myAttr.value = nextAttr.value;
 			}
 		}
